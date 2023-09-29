8f64e8a9bd0f6eefbfa3d0a4834aa32ddadaab3e
sakshiAbsax <sakshi.goyal@absax.com>
2023-09-29 12:47:44 +0530

    first commit

---
+++ src/component/Header.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { Hamburger } from './Hamburger';
+import { NavLink } from 'react-router-dom';
+import logo from '../images/logo.png';
+export const Header=()=>{
+    const [showNavbar, setShowNavbar] = React.useState(false);
+
+    const handleShowNavbar = () => {
+      setShowNavbar(!showNavbar);
+    };
+    return(
+        <>
+    <nav className="navbar bg-light py-2">
+      <div className="container">
+        <div className="logo">
+          <img src={logo}  className='w-100' />
+        </div>
+        <div className="menu-icon" onClick={handleShowNavbar}>
+          <Hamburger />
+        </div>
+        <div className={`nav-elements  ${showNavbar && "active"}`}>
+          <ul>
+            <li>
+              <NavLink className=" fs-5" to="/">Products</NavLink>
+            </li>
+            <li>
+              <NavLink className=" fs-5" to="/blogs">Solutions</NavLink>
+            </li>
+            <li>
+              <NavLink className=" fs-5" to="/projects">Industries</NavLink>
+            </li>
+            <li>
+              <NavLink className=" fs-5" to="/about">Customers</NavLink>
+            </li> 
+            {/* <li>
+              <NavLink className=" fs-5" to="/contact">Contact</NavLink>
+            </li> */}
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+        </>
+    )
+}

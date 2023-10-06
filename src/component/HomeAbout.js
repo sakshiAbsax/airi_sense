1b404598c27c947eafc721014b2dbc1755cfa5f6
sakshiAbsax <sakshi.goyal@absax.com>
2023-10-06 15:59:54 +0530

    update 10/6

--- src/component/HomeAbout.js
+++ src/component/HomeAbout.js
@@ -12,7 +12,7 @@ export const HomeAbout =()=>{
                 <div className="mt-5 pt-5 whatWeDo">
                 <h2> About </h2>
                   <p className="mt-4">Airi sense delivers data-driven & robotic solutions for your logistics automation alongside reliable, modular service concepts. Collaborating with forward-thinking companies, we are committed to setting new standards in warehouse automation to provide future-proof products and solutions. As part of the KUKA Group, our customers trust the competence of our passionate employees – more than 14,000 people working across the globe.</p>
-                 <Link to="/aboutpage"> <div className="btn btn-lg btn-borderd button-1 my-3">What We Do</div> </Link>
+                 <Link to="/about"> <div className="btn btn-lg btn-borderd button-1 my-3">What We Do</div> </Link>
                 </div>
             </Col>
             <Col lg={5} xs={12} md={5}>

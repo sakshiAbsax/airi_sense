5a16981ca5ac82ca9a299ff0043f5f15148cc2e3
sakshiAbsax <sakshi.goyal@absax.com>
2023-10-09 11:42:56 +0530

    Merge branch 'main' of https://github.com/sakshiAbsax/airi_sense

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,73 +1,53 @@
-import React from "react";
-import Container from "react-bootstrap/Container";
-import { Hamburger } from "./Hamburger";
-import { Link, NavLink } from "react-router-dom";
-import logo from "../images/logo.png";
-export const Header = () => {
-  const [showNavbar, setShowNavbar] = React.useState(false);
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import { Hamburger } from './Hamburger';
+import { Link, NavLink } from 'react-router-dom';
+import logo from '../images/logo.png';
+export const Header=()=>{
+    const [showNavbar, setShowNavbar] = React.useState(false);
 
-  const handleShowNavbar = () => {
-    setShowNavbar(!showNavbar);
-  };
-  return (
-    <>
-      <nav className="navbar fixed-top   ">
-        <Container>
-          
-          <div className="header-nav ps-lg-5 ms-lg-5">
-            <Link to="/">
-              <div className="logo ms-lg-5">
-                <img src={logo} className="w-100" />
-              </div>
-            </Link>
-
-            <div className="menu-icon ps-5 ms-5" onClick={handleShowNavbar}>
-              <Hamburger />
-            </div>
-
-            <div className={`nav-elements  ${showNavbar && "active"}`}>
-              <ul className="ms-lg-5  pb-0 mb-0 text-start">
-                <li>
-                  <NavLink className=" fs-6" to="/product">
-                    Products
-                  </NavLink>
-                </li>
-                <li>
-                  <NavLink className=" fs-6" to="/solutions">
-                    Solutions
-                  </NavLink>
-                </li>
-                <li>
-                  <NavLink className=" fs-6" to="/industries">
-                    Industries
-                  </NavLink>
-                </li>
-                <li>
-                  <NavLink className=" fs-6" to="/customer">
-                    Customers
-                  </NavLink>
-                </li>
-                 <li>
-                  <NavLink className=" fs-6" to="/about">
-                    About
-                  </NavLink>
-                </li>
-                {/* <li>
-              <NavLink className=" fs-6" to="/contact">Contact</NavLink>
+    const handleShowNavbar = () => {
+      setShowNavbar(!showNavbar);
+    };
+    return(
+        <>
+    <nav className="navbar bg-light py-2">
+      <Container>
+        <Link to="/">
+      <div className="logo">
+          <img src={logo}  className='w-100' />
+        </div>
+        </Link>
+        <div className="menu-icon" onClick={handleShowNavbar}>
+          <Hamburger />
+        </div>
+        <div className={`nav-elements  ${showNavbar && "active"}`}>
+          <ul>
+          <li>
+              <NavLink className=" fs-5" to="/product">Products</NavLink>
+            </li>
+            <li>
+              <NavLink className=" fs-5" to="/solutions">Solutions</NavLink>
+            </li>
+            <li>
+              <NavLink className=" fs-5" to="/industries">Industries</NavLink>
+            </li>
+            <li>
+              <NavLink className=" fs-5" to="/customer">Customers</NavLink>
+            </li>
+            <li>
+              <NavLink className=" fs-5" to="/about">About</NavLink>
+            </li>
+            {/* <li>
+              <NavLink className=" fs-5" to="/contact">Contact</NavLink>
             </li> */}
-           
-              </ul>
-            
-            </div>
+          </ul>
+        </div>
+      </Container>
+        
+     
+    </nav>
 
-            <div className="ps-lg-5  ps-md-3 ms-lg-3 headerBtn">
-              <a href="/contact" className="text-white btn-md headerBtn  btn-warning btn rounded-pill">Contact-Us</a>
-            </div>
-            
-          </div>
-         
-        </Container>
-      </nav>
-    </>
-  );
-};
+        </>
+    )
+}
--- src/page/Product.js
+++ src/page/Product.js
@@ -1,7 +1,14 @@
 import React from "react";
 import product_bnr from "../images/product_bnr.png";
-import { Col, Row } from "react-bootstrap";
+import { Col, ListGroup, ListGroupItem, Row } from "react-bootstrap";
 import productDiscription from "../images/productDis.webp";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import { Link } from "react-router-dom";
+import download1 from "../images/download1.jpg";
+import download2 from "../images/download2.jpg";
+import donload3 from "../images/donload3.jpg";
+import "./product.css";
 
 export const Product = () => {
   return (
@@ -12,66 +19,224 @@ export const Product = () => {
         </div>
       </section>
 
-      <div className="py-5" style={{ backgroundColor: "#f1f1f1" }}>
-        <div className="container">
-          <Row className="d-flex">
-            <Col lg={7} xs={12} md={7} className="pt-4">
-              <h1
-                className=" fw-bold text-start"
-                style={{ color: "#6f309d", letterSpacing: "6px" }}
-              >
-                About Products
-              </h1>
-              <div className=" pt-2 whatWeDo">
-                <p className="mt-3">
+      <section
+        className="section_padding partnerSec1"
+        style={{ backgroundColor: "#f1f1f1" }}
+      >
+        <div className="container py-5">
+          <div className="row justify-content-between align-items-center">
+            <div className="col col-md-8 col-sm-12 col-lg-6 mb-5">
+              <div className="whatWeDo">
+                <h1
+                  style={{
+                    fontWeight: "600",
+                    color: "#6f309e",
+                    letterSpacing: "3px",
+                  }}
+                  className="text-start list-group-item"
+                >
+                  About Products
+                </h1>
+                <p
+                  className="card-paragraph mt-4"
+                  style={{ textAlign: "justify" }}
+                >
                   Airi-Sense offers you a wide range of traditional and out of
                   the box technologies for automated warehousing that provide
                   quality, flexibility and efficiency.
                 </p>
-                <p className="mt-2">
+                <p
+                  className="card-paragraph mt-4"
+                  style={{ textAlign: "justify" }}
+                >
                   Modular, flexible and software-driven material handling
                   technologies
                 </p>
-                <p className="mt-2">
+                <p
+                  className="card-paragraph mt-4"
+                  style={{ textAlign: "justify" }}
+                >
                   Every customer has different requirements. Our customised
                   solutions with our inhouse products are perfectly harmonized
                   to your needs and guarantee the optimal flow of goods at a
-                  minimal operational cost. 
+                  minimal operational cost.
                 </p>
-                <p className="mt-2">
+                <p
+                  className="card-paragraph mt-4"
+                  style={{ textAlign: "justify" }}
+                >
                   It doesn’t matter what you are looking for, we offer the
                   materials handling technology that perfectly meets your needs
                   from receiving to shipping.
                 </p>
+                <Link to="/about">
+                  {" "}
+                  <div className="btn btn-lg btn-borderd button-1 my-3">
+                    What We Do
+                  </div>{" "}
+                </Link>
               </div>
-            </Col>
-            <Col lg={5} xs={12} md={5} className=" pt-5">
-              <div className="ps-lg-5 ms-3">
-                <img
-                  src={productDiscription}
-                  width="500px"
-                  height="400px"
-                  className="rounded"
-                />
+            </div>
+
+            <div className="col-md-12 col-sm-12 col-lg-4 mb-5">
+              <div className="about_us_img">
+                <img src={productDiscription} width="100%" alt="" />
               </div>
-            </Col>
-          </Row>
+            </div>
+          </div>
         </div>
-      </div>
-
-      <div className="row justify-content-center pt-5 ">
-        <div className="col-lg-8">
-          <h1
-            className=" fw-bold text-center"
-            style={{ color: "#6f309d", letterSpacing: "6px" }}
-          >
-            Our Products
-          </h1>
-        </div>
-      </div>
-
-
-
-    </div>
+      </section>
+        <div className="row justify-content-center pt-5 ">
+            <h1
+              className=" fw-bold text-center"
+              style={{ color: "#6f309d", letterSpacing: "6px" }}
+            >
+              Our Products
+            </h1>
+         
+              <div className="col-lg-4 mx-5">
+                <Card className="me-2 cardhover">
+                  <Card.Img variant="top" src={download1} />
+                  <Card.Body>
+                    <Card.Title>
+                      ​​​​​​​ ​​​​​​​Smart Robotic solutons
+                    </Card.Title>
+                    <ListGroup>
+                      <ListGroup.Item className="text-start list-group-item">
+                        ​Palletizing & De-palletizing
+                      </ListGroup.Item>
+                      <ListGroup.Item className="text-start list-group-item">
+                        Packing Solutions
+                      </ListGroup.Item>
+                      <ListGroup.Item className="text-start list-group-item">
+                        Material Handling
+                      </ListGroup.Item>
+                      <ListGroup.Item className="text-start list-group-item">
+                        Pick & Place
+                      </ListGroup.Item>
+                      <ListGroup.Item className="text-start list-group-item">
+                        Multi-level feeder
+                      </ListGroup.Item>
+                    </ListGroup>
+                  </Card.Body>
+                </Card>
+              </div>
+              <div className="col-lg-4 mx-5">
+                <Card className="me-2 cardhover">
+                  <Card.Img variant="top" src={download2} />
+                  <Card.Body>
+                    <Card.Title>​​Material Transfer Solutions</Card.Title>
+                    <ListGroup>
+                      <ListGroup.Item className="text-start list-group-item list-group-item">
+                        ​Silos
+                      </ListGroup.Item>
+                      <ListGroup.Item className="text-start list-group-item list-group-item">
+                        Fillers
+                      </ListGroup.Item>
+                      <ListGroup.Item className="text-start list-group-item">
+                        Charging Systems
+                      </ListGroup.Item>
+                      <ListGroup.Item className="text-start list-group-item">
+                        Discharging System
+                      </ListGroup.Item>
+                    </ListGroup>
+                  </Card.Body>
+                </Card>
+              </div>
+              <div className="col-lg-4 mx-5 pb-5">
+                <Card className="me-2 cardhover">
+                  <Card.Img variant="top" src={donload3} />
+                  <Card.Body>
+                    <Card.Title>Truck Loading & Unloading Solutions</Card.Title>
+                    <ListGroup className="cardhover">
+                      <ListGroup.Item className="text-start list-group-item">
+                        ​Telescopic Conveyors
+                      </ListGroup.Item>
+                      <ListGroup.Item className="text-start list-group-item">
+                        Expandable Conveyors
+                      </ListGroup.Item>
+                      <ListGroup.Item className="text-start list-group-item">
+                        Expandable Conveyors
+                      </ListGroup.Item>
+                    </ListGroup>
+                  </Card.Body>
+                </Card>
+              </div>
+              <div className="col-lg-4 mx-5">
+                <Card className="me-2 cardhover">
+                  <Card.Img variant="top" src={download1} />
+                  <Card.Body>
+                    <Card.Title>
+                      ​​​​​​​ ​​​​​​​Smart Conveying Solutions
+                    </Card.Title>
+                    ​
+                    <ListGroup>
+                      <ListGroup.Item className="text-start list-group-item list-group-item">
+                        ​Case & Tote Handling Lifts
+                      </ListGroup.Item>
+                      <ListGroup.Item className="text-start list-group-item list-group-item">
+                        Case Conveyor’s – Belt, Modular, PVC, MDR, Roller,
+                        Gravity
+                      </ListGroup.Item>
+                      <ListGroup.Item className="text-start list-group-item list-group-item">
+                        Smart Conveying Solutions
+                      </ListGroup.Item>
+                      <ListGroup.Item className="text-start list-group-item list-group-item">
+                        Pallet Lift
+                      </ListGroup.Item>
+                      <ListGroup.Item className="text-start list-group-item list-group-item">
+                        Turn table - Case, Tote, Pallet
+                      </ListGroup.Item>
+                      <ListGroup.Item className="text-start list-group-item list-group-item">
+                        Cross Transfer System – Case, Tote, Pallet
+                      </ListGroup.Item>
+                      <ListGroup.Item className="text-start list-group-item list-group-item">
+                        Spiral Conveyor’s – Belt, Gravity Roller
+                      </ListGroup.Item>
+                    </ListGroup>
+                  </Card.Body>
+                </Card>
+              </div>
+              <div className="col-lg-4 mx-5">
+                <Card className="me-2 cardhover">
+                  <Card.Img variant="top" src={download2} />
+                  <Card.Body>
+                    <Card.Title>
+                      Cross Transfer System – Case, Tote, Pallet
+                    </Card.Title>
+                    <ListGroup>
+                      <ListGroup.Item className="text-start list-group-item list-group-item">
+                        ​Maintenance Management System
+                      </ListGroup.Item>
+                      <ListGroup.Item className="text-start list-group-item">
+                        Maintenance Management System
+                      </ListGroup.Item>
+                      <ListGroup.Item className="text-start list-group-item">
+                        Energy Management System
+                      </ListGroup.Item>
+                    </ListGroup>
+                  </Card.Body>
+                </Card>
+              </div>
+              <div className="col-lg-4">
+                <Card className="me-2 cardhover">
+                  <Card.Img variant="top" src={donload3} />
+                  <Card.Body>
+                    <Card.Title>
+                      Cloud based - IIoT & Data management Solutions​
+                    </Card.Title>
+                    <ListGroup>
+                      <ListGroup.Item className="text-start list-group-item">
+                        ​Production Data MIS
+                      </ListGroup.Item>
+                      <ListGroup.Item className="text-start list-group-item">
+                        OEE Information system
+                      </ListGroup.Item>
+                    </ListGroup>
+                  </Card.Body>
+                </Card>
+              </div>
+            </div>
+          </div>
   );
 };
